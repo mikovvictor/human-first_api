ac4f03d6913977b3ae58e6a82fbbfb56593d8af8
TRUDER735 <elishaurombo@gmail.com>
2025-07-14 16:15:46 +0200

    ....

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['*'],
+    origin: ['https://humans-first.vercel.app/'],
     allowedHeaders: [
       'Content-Type',
       'Authorization',
