09036b8fc99ec183c8623b7505c5257ef1fb1e1b
TRUDER735 <elishaurombo@gmail.com>
2025-07-14 15:24:09 +0200

    up...

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    credentials: true,
+    origin: ['*','https://humanfirst.org'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+     ],
     methods: [
       'GET',
       "HEAD",
