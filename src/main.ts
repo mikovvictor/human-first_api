5c8ae9e1af6717bafc26954057a0a81874564d74
TRUDER735 <elishaurombo@gmail.com>
2025-07-14 14:38:58 +0200

    main.ts update

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://humansarefirst.org/',
+    origin: '*',
     credentials: true,
+    methods: [
+      'GET',
+      "HEAD",
+      "PUT",
+      'PATCH',
+      'POST',
+      'DELETE',
+      'OPTIONS'
+      ]
+
   });
   await app.listen(process.env.PORT ?? 3000);
 }
